45b4782bd4e36a390712081adfd6ab632109a466
29Lucy <45071093+29Lucy@users.noreply.github.com>
2020-04-16 17:37:28 +0300

    Диофантово уравнение
    Даны числа a, b, c, d, e. Подсчитайте количество таких целых чисел от 0 до 1000 (включительно), которые являются корнями уравнения (ax³+bx²+cx+d)/(x-e)=0, и выведите их количество.
    
    Формат ввода
    
    Вводятся целые числа a, b, c, d и e.
    
    Формат вывода
    
    Выведите ответ на задачу.

--- 3.py
+++ 3.py
@@ -1,9 +1,12 @@
-n = int(input())
-print('+___ ' * n)
-for i in range(1, n + 1):
-    mT = ("|", i, "/ ")
-    a, b, c = mT
-    print(str(a)+str(b), c, end="")
-print()
-print('|__\\ ' * n)
-print('|    ' * n)
+a, b, c = int(input()), int(input()), int(input())
+d, e = int(input()), int(input())
+i = 0
+for x in range(1001):
+    f = (a * x ** 3 + b * x ** 2 + c * x + d)
+    if x != e:
+        g = x - e
+#    (ax³+bx²+cx+d) / (x - e) = 0
+        if (f / g) == 0 and g != 0:
+            i += 1
+
+print(i)
